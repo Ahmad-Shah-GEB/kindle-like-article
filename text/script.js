84906b336e60fc4149b4a012a8b89469819c677a
Ahmad-Shah-GEB <35054221+Ahmad-Shah-GEB@users.noreply.github.com>
2024-02-13 17:33:12 -0500

    Update script.js

--- text/script.js
+++ text/script.js
@@ -1,5 +1,13 @@
 document.getElementById('processBtn').addEventListener('click', function() {
     const inputText = document.getElementById('inputText').value;
-    const escapedText = inputText.replace(/\\/g, '\\\\').replace(/"/g, '\\"').replace(/'/g, "\\'");
+    // Adding backticks before and after the escaped text
+    const escapedText = '`' + inputText.replace(/\\/g, '\\\\').replace(/"/g, '\\"').replace(/'/g, "\\'") + '`';
     document.getElementById('outputText').value = escapedText;
+});
+
+document.getElementById('copyBtn').addEventListener('click', function() {
+    const outputText = document.getElementById('outputText');
+    outputText.select(); // Selects the content of the outputText
+    document.execCommand('copy'); // Copies the selected text to the clipboard
+    alert('Text copied to clipboard'); // Optional: Show a message confirming the copy
 });
